814f6a64c7384819ddbdb6d176f4c9fe60e49527
Qurat238 <q5500407@gmail.com>
2024-03-04 23:30:50 +0500

    done

---
+++ src/client/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./assets/styles/style.css";
+import Typography from './components/Typography';
+
+const App = () => {
+
+  return (
+    <div>
+      <Typography variant='h1'>Hello World</Typography>
+    </div>
+  );
+};
+
+export default App;
